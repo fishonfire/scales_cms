475d8ce190cf0b34a06f10cd6494f8f5f44f21bc
Simon de la Court <info@simondelacourt.nl>
2025-01-28 13:46:55 +0100

    fix align

--- lib/scales_cms_web/components/live/helper_components/block_wrapper.ex
+++ lib/scales_cms_web/components/live/helper_components/block_wrapper.ex
@@ -30,7 +30,7 @@ defmodule ScalesCmsWeb.Components.HelperComponents.BlockWrapper do
     ~H"""
     <div>
       <div class={"#{if !@published, do: "drag-handle"} cursor-move w-full flex justify-between bg-lightGrey py-[8px] transition-all ease-in-out delay-150 duration-300"}>
-        <div class="ml-[12px] text-sm font-semibold leading-[32px]">
+        <div class="align-middle ml-[12px] text-sm font-semibold leading-[32px]">
           {@component.title()}
         </div>
         <div class="flex">
