ccf8f62113b5abb89e72d1221629e252b9ac6817
Menno Jongejan <Menno>
2025-04-01 14:12:26 +0200

    chore: bumped version, changed: removed Thies from maintainers

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ScalesCms.MixProject do
   def project do
     [
       app: :scales_cms,
-      version: "0.1.18",
+      version: "0.1.19",
       elixir: "~> 1.14",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
@@ -109,7 +109,7 @@ defmodule ScalesCms.MixProject do
 
   defp package do
     [
-      maintainers: ["Simon de la Court", "Alexey Pikulik", "Thies van der Zon"],
+      maintainers: ["Simon de la Court", "Alexey Pikulik", "Menno Jongejan"],
       description: "A page builder annex CMS made by Fish on Fire",
       licenses: ["GPL-3.0-or-later"],
       links: %{github: "https://github.com/fishonfire/scales_cms"},
