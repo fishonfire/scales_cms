e0c2b59f11a1602ea2f70c1f577be78b88ced44c
Menno Jongejan <Menno>
2025-04-01 17:34:54 +0200

    fixed: boolean properties now properly shown in api using ecto schema to know which datatype to parse to

--- lib/scales_cms_web/components/live/cms_components/video_component.ex
+++ lib/scales_cms_web/components/live/cms_components/video_component.ex
@@ -5,6 +5,7 @@ defmodule ScalesCmsWeb.Components.CmsComponents.Video do
   use ScalesCmsWeb, :live_component
 
   alias ScalesCms.Cms.Helpers.S3Upload
+  alias ScalesCmsWeb.Components.CmsComponents.Video.VideoProperties
 
   import ScalesCmsWeb.Components.HelperComponents.DrawerComponents
 
@@ -17,17 +18,27 @@ defmodule ScalesCmsWeb.Components.CmsComponents.Video do
     version: "0.0.1"
 
   def serialize(_api_version, block) do
+    raw_properties =
+      Map.put(block.properties, "video_url", get_video_url(block.properties))
+
+    # Cast using the embedded schema
+    casted_properties =
+      %VideoProperties{}
+      |> VideoProperties.changeset(raw_properties)
+      |> Ecto.Changeset.apply_changes()
+      |> Map.from_struct()
+
+    # No need to show null value ID
+    casted_properties = Map.delete(casted_properties, :id)
+
     %{
       id: block.id,
       component_type: block.component_type,
-      properties:
-        Map.merge(block.properties, %{
-          "video_url" => get_video_url(block.properties)
-        })
+      properties: casted_properties
     }
   end
 
-  def get_video_url(block_properties) do
+  defp get_video_url(block_properties) do
     if Map.get(block_properties, "video_path", nil) != nil do
       S3Upload.get_presigned_url_for_display(Map.get(block_properties || %{}, "video_path", nil))
     else
