e3efc29c866c4d69049947905af728fff14231fa
Simon de la Court <info@simondelacourt.nl>
2025-10-24 11:25:59 +0200

    Improve markdown rendering by replacing old converter

--- assets/js/hooks/markdown.js
+++ assets/js/hooks/markdown.js
@@ -1,47 +1,70 @@
-import showdown from 'showdown'
-import TurndownService from 'turndown'
+import TurndownService from "turndown";
+import { marked } from "marked";
+import Trix from "trix";
 
-const converter = new showdown.Converter()
-var turndownService = new TurndownService()
+var turndownService = new TurndownService();
 
-import Trix from "trix"
+turndownService.addRule("paragraph", {
+  filter: "p",
+  replacement: function (content) {
+    return "\n\n" + content + "\n\n";
+  },
+});
 
 // document.addEventListener("trix-before-initialize", () => { /* Change Trix.config if you need */
 Trix.config.blockAttributes.heading2 = {
   tagName: "h2",
   terminal: true,
-  breakOnReturn: true
+  breakOnReturn: true,
 };
 Trix.config.blockAttributes.heading3 = {
   tagName: "h3",
   terminal: true,
-  breakOnReturn: true
+  breakOnReturn: true,
 };
 Trix.config.blockAttributes.heading4 = {
   tagName: "h4",
   terminal: true,
-  breakOnReturn: true
+  breakOnReturn: true,
 };
 Trix.config.blockAttributes.heading5 = {
   tagName: "h5",
   terminal: true,
-  breakOnReturn: true
+  breakOnReturn: true,
 };
 
 export default {
   mounted() {
-    const targetNode = this.el.getElementsByTagName('trix-editor')?.[0]
-    const editor = targetNode.editor
+    const targetNode = this.el.getElementsByTagName("trix-editor")?.[0];
+    const editor = targetNode.editor;
 
+    const inputTarget = document.getElementById(
+      targetNode.id.replace("editor", "content"),
+    );
 
-    const inputTarget = document.getElementById(targetNode.id.replace('editor', 'content'))
-    editor.insertHTML(converter.makeHtml(inputTarget.value))
+    let input = inputTarget.value;
 
+    input = input.replaceAll(/\n/g, "<br>");
 
-    this.el.addEventListener('trix-change', (event) => {
-      const markdown = turndownService.turndown(targetNode.innerHTML)
-      inputTarget.value = markdown
-      inputTarget.dispatchEvent(new Event('input', { bubbles: true }))
-    })
+    const convertedHTML = marked.parse(
+      input.replace(/^[\u200B\u200C\u200D\u200E\u200F\uFEFF]/, ""),
+      {
+        gfm: true,
+        breaks: true,
+      },
+    );
+
+    editor.insertHTML(convertedHTML);
+
+    this.el.addEventListener("trix-change", (event) => {
+      let innerHTML = targetNode.innerHTML;
+
+      innerHTML = innerHTML.replace("<br>", "\n");
+
+      const markdown = turndownService.turndown(innerHTML);
+      inputTarget.value = markdown;
+
+      inputTarget.dispatchEvent(new Event("input", { bubbles: true }));
+    });
   },
-}
+};
