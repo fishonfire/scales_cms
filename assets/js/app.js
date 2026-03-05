dbc0b41a0b46c545c31724e6b1f63c3dad4c836a
Menno Jongejan <Menno>
2026-03-05 12:46:29 +0100

    added: back button and breadcrumb to page builder

--- assets/js/app.js
+++ assets/js/app.js
@@ -51,6 +51,9 @@ topbar.config({ barColors: { 0: "#29d" }, shadowColor: "rgba(0, 0, 0, .3)" });
 window.addEventListener("phx:page-loading-start", (_info) => topbar.show(300));
 window.addEventListener("phx:page-loading-stop", (_info) => topbar.hide());
 
+// Browser back button support
+window.addEventListener("app:back", () => history.back());
+
 // connect if there are any LiveViews on the page
 liveSocket.connect();
 
