f1bff52b09edc4e92ca8b287ce86fafd2bfb45dd
Menno Jongejan <Menno>
2026-03-10 12:16:26 +0100

    removed: dev console logs in dispatch_change_on_update.js

--- assets/js/hooks/dispatch_change_on_update.js
+++ assets/js/hooks/dispatch_change_on_update.js
@@ -1,10 +1,8 @@
 export default DispatchChangeOnUpdate = {
   mounted() {
-    console.log("Mounted");
     this.lastValue = this.el.value;
   },
   updated() {
-    console.log("Value updated, dispatching change event if value has changed");
     if (this.el.value !== this.lastValue) {
       this.lastValue = this.el.value;
       this.el.dispatchEvent(new Event("input", { bubbles: true }));
