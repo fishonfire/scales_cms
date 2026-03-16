dc94eb1024824f0c8c9fccada289eba7306d707b
Menno Jongejan <Menno>
2026-03-16 10:49:29 +0100

    added: start drag animation for items to visually show start drag action, added: on move animation for ghost drag item, changed: drag and dropping is now properly ordered and page blocks cannot be dropped inside component sidebar

--- lib/scales_cms_web/components/live/cms_component_render.ex
+++ lib/scales_cms_web/components/live/cms_component_render.ex
@@ -20,7 +20,7 @@ defmodule ScalesCmsWeb.Components.CmsComponentsRenderer do
     <div
       id={@id}
       data-id={@block.id}
-      class="draggable border-[1px] mb-8 rounded bg-white overflow-hidden"
+      class="draggable border border-slate-200 mb-8 rounded bg-white overflow-hidden"
     >
       <%= if @component do %>
         {@component.render_preview(assigns)}
